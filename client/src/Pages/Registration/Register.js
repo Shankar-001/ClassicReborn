4180cb2cff682ff4585862199a73bd1518059fd1
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-07-07 00:14:49 +0530

    password

--- client/src/Pages/Registration/Register.js
+++ client/src/Pages/Registration/Register.js
@@ -1,5 +1,5 @@
 import { Button, Form, Input, message } from 'antd';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Divider from '../../Components/Divider';
 import { RegisterUser } from '../../apicalls/users';
@@ -10,12 +10,29 @@ const validation = [
   {
     required: true,
     message: 'Fill out this field',
+    validateTrigger: 'onSubmit',
+  },
+];
+
+const passwordValidation = [
+  {
+    required: true,
+    message: 'Please enter a password',
+    validateTrigger: 'onSubmit',
+  },
+  {
+    pattern: /^(?=.*[A-Z])(?=.*\d).{8,}$/,
+    message: 'Passwords must be atleast 8 characters, 1 capital letter, and 1 number.',
+    validateTrigger: 'onSubmit',
   },
 ];
 
 function Register() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [showPasswordError, setShowPasswordError] = useState(false);
+
   const handleFinish = async (values) => {
     try {
       dispatch(SetLoader(true));
@@ -39,24 +56,44 @@ function Register() {
     }
   }, []);
 
+  const handlePasswordChange = () => {
+    if (showPasswordError) {
+      setShowPasswordError(false);
+      form.validateFields(['password']);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      handleFinish(values);
+    } catch (error) {
+      setShowPasswordError(true);
+    }
+  };
+
   return (
-    <div className="h-screen bg-primary flex justify-center items-center ">
-      <div className=" bg-white p-5 rounded w-[550px]">
-        {' '}
-        {/* border border-solid border-gray-800 */}
-        <h1 className=" text-slate-700 text-xl">
-          SellOrSwirl - <span className=" text-gray-700 text-xl">SignUp</span>
+    <div className="h-screen bg-primary flex justify-center items-center">
+      <div className="bg-white p-5 rounded w-[550px]">
+        <h1 className="text-slate-700 text-xl">
+          SellOrSwirl - <span className="text-gray-700 text-xl">SignUp</span>
         </h1>
         <Divider />
-        <Form layout="vertical" onFinish={handleFinish}>
+        <Form layout="vertical" form={form} onFinish={handleSubmit}>
           <Form.Item label="Name" name="name" rules={validation}>
             <Input placeholder="Name" />
           </Form.Item>
           <Form.Item label="Email" name="email" rules={validation}>
             <Input placeholder="Email" />
           </Form.Item>
-          <Form.Item label="Password" name="password" rules={validation}>
-            <Input type="password" placeholder="Password" />
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={passwordValidation}
+            validateStatus={showPasswordError ? 'error' : ''}
+            help={showPasswordError ? passwordValidation[1].message : null}
+          >
+            <Input type="password" placeholder="Password" onChange={handlePasswordChange} />
           </Form.Item>
           <Form.Item label="Confirm Password" name="confirmPassword" rules={validation}>
             <Input type="password" placeholder="Password" />
@@ -67,7 +104,7 @@ function Register() {
           <div className="mt-5 text-center">
             <span className="text-gray-500">
               Already have an account?{' '}
-              <Link to="/login" className=" text-primary">
+              <Link to="/login" className="text-primary">
                 Login
               </Link>
             </span>
@@ -77,4 +114,5 @@ function Register() {
     </div>
   );
 }
+
 export default Register;
