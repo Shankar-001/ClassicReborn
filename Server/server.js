4f4dfd1006c500923aa1aabc680d4b32419da81f
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-08-01 00:04:52 +0530

    deploy

--- Server/server.js
+++ Server/server.js
@@ -18,4 +18,15 @@ app.use('/api/notifications', notificationsRoute);
 
 const port = process.env.PORT || 5000;
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
